9ca1ecc4594f8e994425c96b86f02cee165aa9c9
jermyle2001 <jermyle2001@gmail.com>
2023-06-06 09:33:39 -0500

    Added 36_ValidSudoku.py

---
+++ 36_ValidSudoku.py
@@ -0,0 +1,15 @@
+# LeetCode Problem 36: Valid Sudoku
+# Determine if a 9 x 9 Sudoku Board is valid.
+# Only the filled cells need to be validated
+# according to the following rules:
+# - Each row must contain the digits 1 - 9 
+# without repetition
+# - Each column must contain the digits 1 - 9
+# without repetition
+# - Each of the nine 3 x 3 sub-boxes of the grid
+# must contain the digits 1 - 9 without repetition
+# Note: 
+# - A Sudoku board (partially filled) could be valid
+# but not necessarily solvable
+# - Only the filled cells need to be validated
+# according to the mentioned rules
