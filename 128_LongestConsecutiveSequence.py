4634a99d449c74ea1cc5edf78a21adbf29e70c72
jermyle2001 <jermyle2001@gmail.com>
2023-06-06 15:29:30 -0500

    Started LongestConsecutiveSequence

---
+++ 128_LongestConsecutiveSequence.py
@@ -0,0 +1,11 @@
+# LeetCode Problem 128: Longest Consecutive Sequence
+# Given an unsorted array of integers "nums",
+# return the length of the longest consecutive
+# elemnts sequence.
+# You must write an algorithm that runs in O( n ) time.
+
+def longestConsecutive( nums ):
+    
+    return 0
+
+
