e05198e643cb6a0c8ad4b76c20d8586dc1ff827b
jermyle2001 <jermyle2001@gmail.com>
2023-06-06 15:04:13 -0500

    Finished Valid Sudoku

--- 36_ValidSudoku.py
+++ 36_ValidSudoku.py
@@ -17,65 +17,50 @@
 # -------------------------------------------------------------
 # SOLUTION 1: Brute Force
 #   Check the rows, columns, and squares with sub-functions. The
-#   board is not big enough require a faster solution.
+#   board is not big enough require a faster solution. We can use 
+#   sets to help us identify if any duplicates exist or not.
 def isValidSudoku( board ):
     # Number of rows and columns is predefined, making
     # our task much easier. Iterate through each row
     # and column, as well as square, and validate each
     # requirement of the board.
-    for i in range( 9 ):
-        if checkRowValid( board, i ) != True:
-            return False
+    rows = [ set( ) for x in range( 9 ) ]
+    cols = [ set( ) for x in range( 9 ) ]
+    squares = [ [ set( ) for x in range( 3 ) ] for y in range( 3 ) ]
     
-    for j in range( 9 ):
-        if checkColValid( board, j ) != True:
-            return False
-        
-    for i in range( 3 ):
-        for j in range( 3 ):
-            if checkSquareValid( board, i, j ) != True:
+    for y in range( 9 ):
+        for x in range( 9 ):
+            # Python is row-major. Get the value of the current
+            # cell.
+            cell_value = board[ y ][ x ]
+            
+            # Check if cell_value is a number, or not "."
+            if cell_value == ".":
+                continue
+            
+            # Now check if the value exists in its current row,
+            # column, or square already
+            if cell_value in rows[ y ]:
                 return False
+            if cell_value in cols[ x ]:
+                return False
+            # Use the floor operator to identify which square the
+            # current x and y value belong to
+            if cell_value in squares[ y // 3 ][ x // 3 ]:
+                return False
+
+            # This is our first time encountering the value in the
+            # row, column, and square. Add it to our set.
+            rows[ y ].add( cell_value )
+            cols[ x ].add( cell_value )
+            squares[ y // 3 ][ x // 3 ].add( cell_value )
 
     return True
 
 
-def checkRowValid( board, row ):
-    curr_row = board[ row ][ : ]
-    if curr_row.count( "." ) == 9:
-        return False
-    for i in range( 9 ):
-        if curr_row[ i ] != ".":
-            if curr_row.count( curr_row[ i ] )  > 1:
-                return False
-    return True
 
-
-def checkColValid( board, col ):
-    curr_col = board[ : ][ col ]
-    for i in range( 9 ):
-        if curr_col[ i ] != ".":
-            if curr_col.count( curr_col[ i ] )  > 1:
-                return False
-    return True
-
-
-def checkSquareValid( board, row, col ):
-    # Note: Multi-Axis Slicing is not possible with lists,
-    # so we have to do it mildly manually
-    curr_square = [ ]
-    for i in range( 3 ):
-        for j in range( 3 ):
-            curr_square.append( board[ i ][ j ] )
-    
-    for i in range( 9 ):
-        if curr_square[ i ] != ".":
-            if curr_square.count( curr_square[ i ] )  > 1:
-                return False
-    
-    return True
-
-
-
+# -------------------------------------------------------------
+# TEST SUITE available below.
 test_board1 = [["5","3",".",".","7",".",".",".","."]
 ,["6",".",".","1","9","5",".",".","."]
 ,[".","9","8",".",".",".",".","6","."]
@@ -108,6 +93,8 @@ test_board3 = [[".",".","4",".",".",".","6","3","."],
 
 result1 = isValidSudoku( test_board1 )
 result2 = isValidSudoku( test_board2 )
+result3 = isValidSudoku( test_board3 )
 
 print( "Test 1 - Expected Value: True | Actual Value:", result1 )
 print( "Test 2 - Expected Value: False | Actual Value:", result2 )
+print( "Test 3 - Expected Value: False | Actual Value:", result3 )
