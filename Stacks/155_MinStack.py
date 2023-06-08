201736a467cbc743213ca5966973499aabc84c50
jermyle2001 <jermyle2001@gmail.com>
2023-06-08 09:54:10 -0500

    Start Min Stack

---
+++ Stacks/155_MinStack.py
@@ -0,0 +1,14 @@
+# LeetCode Problem 155: Min Stack
+# Design a stakc that supports push, pop, top
+# and retrieving the minimum element in constant
+# time.
+# Implement the 'MinStack' class:
+#   MinStack( ) initializes the object
+#   void push( int val ) pushes the element
+#       'val' onto the stack
+#   int top( ) gets the top element of the 
+#       stack
+#   int getMin( ) retrieves the minimum element
+#       in the stack
+# You must implement a solution with O( 1 ) time
+# complexity for each function
